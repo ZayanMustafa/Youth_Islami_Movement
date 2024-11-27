a261bf31420fe827b7e564f1e0157b3f1ba8df59
Zayan Mustafa <mustafazayyan13@gmail.com>
2024-11-27 18:25:48 +0500

    Update a Project

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,14 @@ import RegisterForm from "./Auth/Signin";
 import ReadMoreAboutUs from "./components/readmoreaboutus";
 import NotFound from "./components/NotFound";
 import {NextUIProvider} from '@nextui-org/react'
-
+// import { app } from "./utills/firebase";
 export default function App() {
   return (
     <>
     <NextUIProvider> 
       <BrowserRouter>
         <Routes>
+          {/* console.log(<app/>) */}
           <Route path="/" element={<Home />} ></Route>
           <Route path="/readmore" element={<ReadMoreAboutUs />}></Route>
           <Route path="/signin" element={<RegisterForm />}></Route>
--- src/utills/firebase.js
+++ src/utills/firebase.js
@@ -1,23 +1,25 @@
 
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth, 
-         signInWithEmailAndPassword, 
-         createUserWithEmailAndPassword,
-         GoogleAuthProvider, 
-         signOut,
-         onAuthStateChanged } from "firebase/auth";
+import {
+    getAuth,
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
+    GoogleAuthProvider,
+    signOut,
+    onAuthStateChanged
+} from "firebase/auth";
 
-const firebaseConffig = {
-    apiKey: "AIzaSyCfq5jMywCUO-sNrFqkzVe0_zpQxx4YWOM",
-    authDomain: "some-projects-of-zayyan.firebaseapp.com",
-    databaseURL: "https://some-projects-of-zayyan-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "some-projects-of-zayyan",
-    storageBucket: "some-projects-of-zayyan.firebasestorage.app",
-    messagingSenderId: "940547270190",
-    appId: "1:940547270190:web:6c79c1818f01af44f221bf",
-    measurementId: "G-8J5PVEDEC5"
+const firebaseConfig = {
+    apiKey: "AIzaSyCkkiOGLP7Y-2m5OD8FVA4Zpaqe2NhHfy4",
+    authDomain: "youthislami-movement.firebaseapp.com",
+    projectId: "youthislami-movement",
+    storageBucket: "youthislami-movement.firebasestorage.app",
+    messagingSenderId: "816523599284",
+    appId: "1:816523599284:web:534183d7dc7502e920a6e1",
+    measurementId: "G-41ZNP4NJR1"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -25,9 +27,9 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
 export {
-    signInWithEmailAndPassword, 
+    signInWithEmailAndPassword,
     createUserWithEmailAndPassword,
-    GoogleAuthProvider, 
+    GoogleAuthProvider,
     signOut,
     onAuthStateChanged,
     auth,
